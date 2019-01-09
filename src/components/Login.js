490461780264b6d0db0a24bf1a3042588563477a
Sk Omar Faruque <sohag@lolobyte.com>
2019-01-09 23:51:49 +0600

    icons added

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,11 +5,11 @@ import { connect } from 'react-redux'
 import SearchBox from './SearchBox';
 import '../style.css'
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
-class Login extends React.Component{
+class Login extends React.Component {
   static defaultProps = {
     center: {
-      lat: 23.8103,
-      lng: 90.4125
+      lat: 52.070499,
+      lng: 4.3007
     },
     zoom: 11
   };
@@ -20,9 +20,14 @@ class Login extends React.Component{
       mapInstance: null,
       mapApi: null,
       places: [],
+      center: {
+        lat: 52.070499,
+        lng: 4.3007
+      },
+      zoom: 11
     };
   }
-  componentWillMount () {
+  componentWillMount() {
     document.title = "Home";
   }
   handleApiLoaded = (map, maps) => {
@@ -33,27 +38,45 @@ class Login extends React.Component{
       mapApi: maps,
       parkData: []
     });
+    this.populateMarker(this.state.center)
   };
-  addPlace = async(place) => {
-    console.log(place[0].geometry.location.lat())
+  addPlace = async (place) => {
+   
+    this.setState({ places: place });
+    let newState = {...this.state}
+    newState.center.lat = place[0].geometry.location.lat()
+    newState.center.lng = place[0].geometry.location.lng()
+    this.populateMarker(newState)
+  };
+  populateMarker = async(place) => {
+    console.log("lat", place.lat, "lng", place.lng)
     var service = new this.state.mapApi.places.PlacesService(this.state.mapInstance);
     const pResult = await service.nearbySearch({
-      location: {lat: place[0].geometry.location.lat(), lng:  place[0].geometry.location.lng()},
+      // location: { lat: place[0].geometry.location.lat(), lng: place[0].geometry.location.lng() },
+      location: this.state.center,
       radius: 1000,
       type: ['parking']
     }, (results, status) => {
+      console.log(results)
       for (var i = 0; i < results.length; i++) {
-        // createMarker(results[i]);
+        var icon = {
+          url: "../assets/menu_icons/google_parking.png", // url
+          scaledSize: new this.state.mapApi.Size(30, 30), // scaled size
+          origin: new this.state.mapApi.Point(0,0), // origin
+          anchor: new this.state.mapApi.Point(0, 0) // anchor
+      };
         new this.state.mapApi.Marker({
           map: this.state.mapInstance,
-          position: results[i].geometry.location
+          position: results[i].geometry.location,
+          animation: this.state.mapApi.Animation.DROP,
+          icon:icon,
+          // size: new this.state.mapInstance.Size(20, 32),
+          size: new this.state.mapApi.Size(10, 10),
         });
       }
     });
-    console.log(this.state)
-    this.setState({ places: place });
-  };
-  
+  }
+
   createMapOptions = (maps) => {
     return {
       mapTypeControl: true,
@@ -66,15 +89,15 @@ class Login extends React.Component{
     } = this.state;
     return (
       <div className="map-canvas">
-       
+
 
         <GoogleMapReact
-        bootstrapURLKeys={{
+          bootstrapURLKeys={{
             key: "AIzaSyAyIvCIJ8K57oZ0Hra-TPJWOAP8gjiJ7E8",
             libraries: ['places', 'geometry'],
           }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+          defaultCenter={this.state.center}
+          defaultZoom={this.state.zoom}
           options={this.createMapOptions()}
           yesIWantToUseGoogleMapApiInternals
           onGoogleApiLoaded={({ map, maps }) => this.handleApiLoaded(map, maps)}
@@ -87,7 +110,7 @@ class Login extends React.Component{
         </GoogleMapReact>
         {mapApiLoaded && <SearchBox map={mapInstance} mapApi={mapApi} addplace={this.addPlace} />}
       </div>
-    
+
     )
   }
 }
@@ -99,5 +122,5 @@ const mapDispatchToProps = dispatch => ({
   login: (token) => dispatch({ type: 'Store', token }),
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);  
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
 // export default Login
