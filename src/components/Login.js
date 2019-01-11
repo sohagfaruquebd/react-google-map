63e0c6ba16b5235131929b7e2be2c231cb3e1e24
Sk Omar Faruque <sohag@lolobyte.com>
2019-01-12 01:51:57 +0600

    responsive

--- src/components/Login.js
+++ src/components/Login.js
@@ -152,36 +152,7 @@ class Login extends React.Component {
           />
         </GoogleMapReact>
         {mapApiLoaded && <SearchBox map={mapInstance} mapApi={mapApi} addplace={this.addPlace} />}
-        {this.state.isActiveModal && <div className="Modal is-active">
-  <div className="modal-background"></div>
-  <div className="modal-content">
-  <div className="box">
-      <article className="media">
-        <div className="media-left">
-          <figure className="image is-64x64">
-            <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image"/>
-          </figure>
-        </div>
-        <div className="media-content">
-          <div className="content">
-            <p>
-              <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
-              <br/>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-            </p>
-          </div>
-          <nav className="level is-mobile">
-            <div className="level-left">
-             
-                
-            </div>
-          </nav>
-        </div>
-      </article>
-    </div>
-  </div>
-  <button className="modal-close is-large" aria-label="close" onClick={() => this.closeModal()}></button>
-</div>}
+  
       </div>
 
     )
