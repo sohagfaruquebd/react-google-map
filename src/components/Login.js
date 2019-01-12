db373de636ebde7522b1a47f7d63c83ade817218
Sk Omar Faruque <sohag@lolobyte.com>
2019-01-13 02:57:46 +0600

    icons added

--- src/components/Login.js
+++ src/components/Login.js
@@ -31,7 +31,7 @@ class Login extends React.Component {
         lng: 4.3007
       },
       zoom: 11,
-      placeName: "" 
+      placeName: ""
     };
   }
   componentWillMount() {
@@ -48,76 +48,104 @@ class Login extends React.Component {
     this.populateMarker(this.state.center)
   };
   addPlace = async (place) => {
-   
+
     this.setState({ places: place });
-    let newState = {...this.state}
+    let newState = { ...this.state }
     newState.center.lat = place[0].geometry.location.lat()
     newState.center.lng = place[0].geometry.location.lng()
     this.populateMarker(newState)
   };
-  populateMarker = async(place) => {
+  populateMarker = async (place) => {
     console.log("lat", place.lat, "lng", place.lng)
-   
+
     var service = new this.state.mapApi.places.PlacesService(this.state.mapInstance);
     const pResult = await service.nearbySearch({
       // location: { lat: place[0].geometry.location.lat(), lng: place[0].geometry.location.lng() },
       location: this.state.center,
       radius: 1000,
       type: ['parking']
-    }, async(results, status) => {
+    }, async (results, status) => {
       results = [...results]
       console.log("google palce", results)
       let username = "imentstralliatherederess"
       let password = "9dea22ce6e159f2f7c9f709e7359660117006e8c"
-      let url = "/newGeoIndex?lat=52.07725093804627&lon=4.345052540302277&radius=200&relation=contains&include_docs=true";
+      let url = `/newGeoIndex?lat=${place.lat}&lon=${place.lng}&radius=200&relation=contains&include_docs=true`;
       var basicAuth = 'Basic ' + btoa(username + ':' + password);
       console.log(basicAuth)
 
-      let {data} = await axios.get(url, {headers: { 'Authorization': basicAuth}});
+      let { data } = await axios.get(url, { headers: { 'Authorization': basicAuth } });
       console.log(data.rows)
-      results.push(data.rows[0])
+      await data.rows.map(ibmData => {
+        results.push(ibmData)
+      })
+
       console.log("after push", results)
       // let ibmPlace = await fetch();
-      console.log("marker pionts",results)
-      let markers =[]
+      console.log("marker pionts", results)
+      let markers = []
       for (var i = 0; i < results.length; i++) {
-        var icon = {
+        var myLatLng = { lat: -25.363, lng: 131.044 };
+        let icon = {
           url: "../assets/menu_icons/google_parking.png", // url
           scaledSize: new this.state.mapApi.Size(30, 30), // scaled size
-          origin: new this.state.mapApi.Point(0,0), // origin
+          origin: new this.state.mapApi.Point(0, 0), // origin
           anchor: new this.state.mapApi.Point(0, 0) // anchor
-      };
-       let marker = new this.state.mapApi.Marker({
+        };
+        if (results[i].doc) {
+          let type = results[i].doc.metadata.Location_Class
+          icon = {
+            url: `../assets/marker_icons/an_${type}_green.png`, // url
+            scaledSize: new this.state.mapApi.Size(30, 30), // scaled size
+            origin: new this.state.mapApi.Point(0, 0), // origin
+            anchor: new this.state.mapApi.Point(0, 0) // anchor
+          };
+          myLatLng = { lat: results[i].doc.geometry.coordinates[1], lng: results[i].doc.geometry.coordinates[0] };
+        } else {
+          myLatLng = { lat: results[i].geometry.location.lat(), lng: results[i].geometry.location.lng() }
+        }
+
+        let marker = new this.state.mapApi.Marker({
           map: this.state.mapInstance,
-          // position: results[i].geometry.location,
-          position: results[i].geometry.coordinates ? results[i].geometry.coordinates  : results[i].geometry.location,
+          position: myLatLng,
           animation: this.state.mapApi.Animation.DROP,
-          icon:icon,
-          // size: new this.state.mapInstance.Size(20, 32),
+          icon: icon,
           size: new this.state.mapApi.Size(10, 10),
           markerInfo: results[i],
           // map: this.state.mapApi
         });
         marker.addListener('click', (d) => {
-          
+
           let markerInfo = marker.markerInfo
-          this.setState({placeName: marker.markerInfo.name})
-         let imageUrl = markerInfo.photos ? markerInfo.photos[0].getUrl() : "";
-         let openStatus = markerInfo.opening_hours ? markerInfo.opening_hours.open_now ? "Open" : "Gesloten" : ""
+          if (markerInfo.doc) {
+            console.log(marker)
           var infowindow = new this.state.mapApi.InfoWindow({
-            content: '<div id="content">'+
-            '<div id="siteNotice">'+
-            '</div>'+
-            '<div id="bodyContent">'+
-            '<div class="post-container"><div class="post-thumb"><img src="'+imageUrl+' height="200" width="200" /></div><div class="post-content"><h3 class="post-title">'+markerInfo.name+'</h3><h3 class="post-title">'+markerInfo.vicinity+'</h3><h3 class="post-title">Status: '+openStatus+'</h3><h3 class="post-title">Type: Parkerrgarage</h3></div></div><button class="custombutton button5" href="#">Take Me There</button>'+
-            '</div>'+
-            '</div>'
+            content: '<div id="content">' +
+              '<div id="siteNotice">' +
+              '</div>' +
+              '<div id="bodyContent">' +
+              '<div class="post-container"><div class="post-thumb"><img src="' + marker.icon + ' height="200" width="200" /></div><div class="post-content"><h3 class="post-title">'+ '</h3><h3 class="post-title">' + '</h3><h3 class="post-title">Status: ' + '</h3><h3 class="post-title">Type: Parkerrgarage</h3></div></div><button class="custombutton button5" href="#">Take Me There</button>' +
+              '</div>' +
+              '</div>'
           });
+          } else {
+            let imageUrl = markerInfo.photos ? markerInfo.photos[0].getUrl() : "";
+          let openStatus = markerInfo.opening_hours ? markerInfo.opening_hours.open_now ? "Open" : "Gesloten" : ""
+          var infowindow = new this.state.mapApi.InfoWindow({
+            content: '<div id="content">' +
+              '<div id="siteNotice">' +
+              '</div>' +
+              '<div id="bodyContent">' +
+              '<div class="post-container"><div class="post-thumb"><img src="' + imageUrl + ' height="200" width="200" /></div><div class="post-content"><h3 class="post-title">' + markerInfo.name + '</h3><h3 class="post-title">' + markerInfo.vicinity + '</h3><h3 class="post-title">Status: ' + openStatus + '</h3><h3 class="post-title">Type: Parkerrgarage</h3></div></div><button class="custombutton button5" href="#">Take Me There</button>' +
+              '</div>' +
+              '</div>'
+          });
+          }
+          
           infowindow.open(this.state.mapApi, marker);
           console.log(markerInfo)
         }
         );
-       
+
         markers.push(marker)
       }
     });
@@ -132,7 +160,7 @@ class Login extends React.Component {
   }
   closeModal = () => {
     console.log("click close modal")
-    this.setState({isActiveModal: false})
+    this.setState({ isActiveModal: false })
   }
   render() {
     const {
@@ -160,7 +188,7 @@ class Login extends React.Component {
           />
         </GoogleMapReact>
         {mapApiLoaded && <SearchBox map={mapInstance} mapApi={mapApi} addplace={this.addPlace} />}
-  
+
       </div>
 
     )
